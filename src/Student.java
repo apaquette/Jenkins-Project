98fc78935b6dc85494c655aa96423b4da2662de9
Alex <alexandre.d.paquette@gmail.com>
2023-11-29 10:54:12 +0000

    Added Student class and Test file

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+/**
+ * Created by meudecc on 15/02/2017.
+ */
+public class Student {
+
+    public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
+
+    public AttendanceGrade getAttendanceGrade(int attendance) {
+        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 50) return AttendanceGrade.POOR;
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance <= 90) return AttendanceGrade.GOOD;
+        return AttendanceGrade.VERY_GOOD;
+    }
+}
