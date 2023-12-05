3e1e0a12b337fa7a7f5e14fef3d8eea82d67ecd0
Alex <alexandre.d.paquette@gmail.com>
2023-12-05 11:42:36 +0000

    Refactor: Moved illegal argument check at the top of the conditional chain

--- src/Student.java
+++ src/Student.java
@@ -6,8 +6,8 @@ public class Student {
     public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
-        if(attendance == 0) return AttendanceGrade.ABSENT;
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if(attendance == 0) return AttendanceGrade.ABSENT;
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
         if (attendance < 50) return AttendanceGrade.POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
