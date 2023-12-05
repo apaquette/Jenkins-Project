5ce95042ae0cb356a982d5b5dc260710d9a34f82
Alex <alexandre.d.paquette@gmail.com>
2023-12-05 11:44:55 +0000

    Code: Commented out if statement under 50. Now passes lowAverage() test

--- src/Student.java
+++ src/Student.java
@@ -9,7 +9,7 @@ public class Student {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
         if(attendance == 0) return AttendanceGrade.ABSENT;
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
-        if (attendance < 50) return AttendanceGrade.POOR;
+        //if (attendance < 50) return AttendanceGrade.POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
         if (attendance <= 90) return AttendanceGrade.GOOD;
         return AttendanceGrade.VERY_GOOD;
